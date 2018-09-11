69679cb3a78436ece57d27ac128915e4c2fd691f
Yuji Hanamura <yuji.developer@gmail.com>
2018-09-11 23:30:07 +0900

    Cosmetic changes

--- spec/operator_recordable/store_spec.rb
+++ spec/operator_recordable/store_spec.rb
@@ -12,19 +12,19 @@ RSpec.describe OperatorRecordable::Store do
   describe ".fetch_class" do
     subject { described_class.fetch_class(name) }
 
-    context ":thread_store" do
+    context "when :thread_store is passed" do
       let(:name) { :thread_store }
 
       it { is_expected.to eq OperatorRecordable::ThreadStore }
     end
 
-    context ":request_store" do
+    context "when :request_store is passed" do
       let(:name) { :request_store }
 
       it { is_expected.to eq OperatorRecordable::RequestStore }
     end
 
-    context ":current_attributes_store" do
+    context "when :current_attributes_store is passed" do
       let(:name) { :current_attributes_store }
 
       if defined? ::ActiveSupport::CurrentAttributes
@@ -34,7 +34,7 @@ RSpec.describe OperatorRecordable::Store do
       end
     end
 
-    context ":unknown_store" do
+    context "when :unknown_store is passed" do
       let(:name) { :unknown_store }
 
       it { expect { subject }.to raise_error KeyError }
