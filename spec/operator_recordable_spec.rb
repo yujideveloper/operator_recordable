2fb4d1d90fd63ebbf2e05cdb7b5f8066a15366dd
Yuji Hanamura <yuji.developer@gmail.com>
2022-08-18 22:40:22 +0900

    Test with paranoia

--- spec/operator_recordable_spec.rb
+++ spec/operator_recordable_spec.rb
@@ -2,6 +2,7 @@
 
 require "spec_helper"
 require "discard"
+require "paranoia"
 
 ActiveRecord::Base.configurations = {
   "test" => { "adapter" => "sqlite3", "database" => ":memory:" }
@@ -34,6 +35,17 @@ class CreateAllTables < ActiveRecord::Migration::Current
       t.integer :updated_by
       t.datetime :discarded_at
       t.integer :discarded_by
+    end
+    create_table(:comments) do |t|
+      t.string :title
+      t.string :body
+      t.integer :lock_version
+      t.datetime :created_at
+      t.integer :created_by
+      t.datetime :updated_at
+      t.integer :updated_by
+      t.datetime :deleted_at
+      t.integer :deleted_by
     end
   end
 end
@@ -77,6 +89,12 @@ class Post < ApplicationRecord
   record_operator_on :create, :update, :discard
 end
 
+class Comment < ApplicationRecord
+  acts_as_paranoid
+
+  record_operator_on :create, :update, :destroy
+end
+
 RSpec.describe OperatorRecordable do
   it "has a version number" do
     expect(OperatorRecordable::VERSION).not_to be nil
@@ -118,25 +136,50 @@ RSpec.describe OperatorRecordable do
   end
 
   describe "deleter" do
-    after do
-      Account.delete_all
-      Operator.delete_all
+    context "with SoftDeletable" do
+      after do
+        Account.delete_all
+        Operator.delete_all
+      end
+
+      it "should be assigned deleter" do
+        operator1 = Operator.create!(name: "op1")
+        OperatorRecordable.operator = operator1
+        account = Account.create!
+        expect(account.updated_by).to eq operator1.id
+        expect(account.updater).to eq operator1
+        operator2 = Operator.create!(name: "op2")
+        OperatorRecordable.operator = operator2
+        account.destroy!
+        expect(account.deleted_by).to eq operator2.id
+        expect(account.deleter).to eq operator2
+        account.reload
+        expect(account.deleted_by).to eq operator2.id
+        expect(account.deleter).to eq operator2
+      end
     end
 
-    it "should be assigned deleter" do
-      operator1 = Operator.create!(name: "op1")
-      OperatorRecordable.operator = operator1
-      account = Account.create!
-      expect(account.updated_by).to eq operator1.id
-      expect(account.updater).to eq operator1
-      operator2 = Operator.create!(name: "op2")
-      OperatorRecordable.operator = operator2
-      account.destroy!
-      expect(account.deleted_by).to eq operator2.id
-      expect(account.deleter).to eq operator2
-      account.reload
-      expect(account.deleted_by).to eq operator2.id
-      expect(account.deleter).to eq operator2
+    context "with paranoia" do
+      after do
+        Comment.delete_all
+        Operator.delete_all
+      end
+
+      it "should be assigned deleter" do
+        operator1 = Operator.create!(name: "op1")
+        OperatorRecordable.operator = operator1
+        comment = Comment.create!
+        expect(comment.updated_by).to eq operator1.id
+        expect(comment.updater).to eq operator1
+        operator2 = Operator.create!(name: "op2")
+        OperatorRecordable.operator = operator2
+        comment.destroy!
+        expect(comment.deleted_by).to eq operator2.id
+        expect(comment.deleter).to eq operator2
+        comment.reload
+        expect(comment.deleted_by).to eq operator2.id
+        expect(comment.deleter).to eq operator2
+      end
     end
   end
 
